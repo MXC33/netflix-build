ced6a0cc841e7c4308ad07a3fc0c652c34afaac9
mxc33 <113206163+mxc33@users.noreply.github.com>
2024-10-03 23:31:57 +0200

    Stop tracking src/firebase.js

--- src/firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyC7xZ_Ie74Ad62qZ327Mi5EA7Aim-jTUf0",
-  authDomain: "netflix-clone-mxc.firebaseapp.com",
-  projectId: "netflix-clone-mxc",
-  storageBucket: "netflix-clone-mxc.appspot.com",
-  messagingSenderId: "135606192899",
-  appId: "1:135606192899:web:943fe2d840387857a43736",
-};
