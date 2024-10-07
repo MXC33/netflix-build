c698436edd4d0e25e5646c0d5f2982f7591edc6c
mxc33 <113206163+mxc33@users.noreply.github.com>
2024-10-07 12:07:38 +0200

    PlansScreen component by updating Firestore queries for fetching subscriptions and products, fixing loadCheckout logic, and adding UI improvements for the disabled subscription plan.
    - Enhanced Firestore integration for subscription fetching with useEffect.
    
    - Fixed loadCheckout function to handle sessions and errors properly.
    
    - Updated UI to disable the subscription button for the current package.
    
    - Added .plansScreen_plan--disabled styling to indicate disabled plans in the UI.

--- src/screens/PlansScreen.js
+++ src/screens/PlansScreen.js
@@ -1,6 +1,14 @@
 import React, { useEffect, useState } from "react";
 import "./PlansScreen.css";
-import { collection, query, where, getDocs } from "firebase/firestore";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  doc,
+  addDoc,
+  onSnapshot,
+} from "firebase/firestore";
 import db from "../firebase";
 import { useSelector } from "react-redux";
 import { selectUser } from "../features/userSlice";
@@ -9,6 +17,32 @@ import { loadStripe } from "@stripe/stripe-js";
 function PlansScreen() {
   const [products, setProducts] = useState([]);
   const user = useSelector(selectUser);
+  const [subscription, setSubscription] = useState(null);
+
+  useEffect(() => {
+    const fetchSubscription = async () => {
+      try {
+        const customerRef = doc(db, "customers", user.uid);
+        const subscriptionsRef = collection(customerRef, "subscriptions");
+        const querySnapshot = await getDocs(subscriptionsRef);
+
+        querySnapshot.forEach(async (subscription) => {
+          setSubscription({
+            role: subscription.data().role,
+            current_period_end: subscription.data().current_period_end.seconds,
+            current_period_start:
+              subscription.data().current_period_start.seconds,
+          });
+        });
+      } catch (error) {
+        console.error("Error fetching subscription: ", error);
+      }
+    };
+
+    if (user?.uid) {
+      fetchSubscription();
+    }
+  }, [user?.uid]);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -37,42 +71,72 @@ function PlansScreen() {
     fetchProducts();
   }, []);
 
-  console.log(products);
+  // console.log(products);
+  // console.log(subscription);
 
   const loadCheckout = async (priceId) => {
-    const docRef = await db
-      .collection("costumers")
-      .doc(user.uid.collection(""))
-      .add({
-        price: priceId,
-        sucess_url: window.location.origin,
-        cancel_url: window.location.origin,
-      });
+    const customerRef = doc(db, "customers", user.uid);
 
-    docRef.onSnapShit(async (snap) => {
-      const { error, sessionId } = snap.data();
-      if (error) {
-        alert(`An error occured: ${error.message}`);
-      }
-      if (sessionId) {
-        const stripe = await loadStripe();
-      }
-    });
+    try {
+      const checkoutRef = await addDoc(
+        collection(customerRef, "checkout_sessions"),
+        {
+          price: priceId,
+          success_url: window.location.origin,
+          cancel_url: window.location.origin,
+        }
+      );
+
+      onSnapshot(checkoutRef, async (snap) => {
+        const { error, sessionId } = snap.data();
+        if (error) {
+          alert(`An error occurred: ${error.message}`);
+        }
+        if (sessionId) {
+          const stripe = await loadStripe(
+            "pk_test_51Q6bsK02q0XRsQMHQAJVtKVfY0Rtxq9bWCpYUfXuh9FTn7RjskHVv2s1Pl4N2bhsqO03o9czJiJ06lxBpYH1gInM00dUokyH58"
+          );
+          stripe.redirectToCheckout({ sessionId });
+        }
+      });
+    } catch (err) {
+      console.error("Error adding document: ", err);
+    }
   };
 
   return (
     <div className="plansScreen">
+      <br />
+      {subscription && (
+        <p>
+          Renewal Date:
+          {new Date(
+            subscription?.current_period_end * 1000
+          ).toLocaleDateString()}
+        </p>
+      )}
       {Object.entries(products).map(([productId, productData]) => {
+        const isCurrentPackage = productData.name
+          ?.toLowerCase()
+          .includes(subscription?.role);
+
         return (
-          <div className="plansScreen_plan" key={productId}>
-            {" "}
-            {}
+          <div
+            className={`${
+              isCurrentPackage && "plansScreen_plan--disabled"
+            } plansScreen_plan`}
+            key={productId}
+          >
             <div className="planScreen_info">
               <h5>{productData.name}</h5>
               <h6>{productData.description}</h6>
             </div>
-            <button onClick={() => loadCheckout(productData.prices.priceId)}>
-              Subscribe
+            <button
+              onClick={() =>
+                !isCurrentPackage && loadCheckout(productData.prices.priceId)
+              }
+            >
+              {isCurrentPackage ? "Current Package" : "Subscribing"}
             </button>
           </div>
         );
